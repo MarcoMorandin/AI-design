a68e1ce3f4b0b939f92c94b78d23578cae2aa05f
marcomorandin <emmeemmenove@gmail.com>
2025-04-29 13:16:37 +0200

    added tools

--- SummarizationAgent/agent.py
+++ SummarizationAgent/agent.py
@@ -1,16 +1,16 @@
 import os, sys
 
-from get_text.get_text import get_text
-from summarizer_type.get_correct_format_prompt import (
+from tools.get_text.get_text import getTextFromPdf, getTextFromVideo
+from tools.summarizer_type.get_correct_format_prompt import (
     get_correct_format_prompt,
 )
-from summarizer_type.get_summarize_chunk_prompt import (
+from tools.summarizer_type.get_summarize_chunk_prompt import (
     get_prompt_to_summarize_chunk,
 )
-from summarizer_type.get_final_summary_prompt import (
+from tools.summarizer_type.get_final_summary_prompt import (
     get_final_summary_prompt,
 )
-from chunker.chunker_tool import get_chunks
+from tools.chunker.chunker_tool import get_chunks
 
 
 from trento_agent_sdk.tool.tool_manager import ToolManager
@@ -20,7 +20,8 @@ import asyncio
 import streamlit as st
 
 manager = ToolManager()
-manager.add_tool(get_text)
+manager.add_tool(getTextFromPdf)
+manager.add_tool(getTextFromVideo)
 manager.add_tool(get_chunks)
 manager.add_tool(get_correct_format_prompt)
 manager.add_tool(get_prompt_to_summarize_chunk)
@@ -28,7 +29,7 @@ manager.add_tool(get_final_summary_prompt)
 
 summarizerAgent = Agent(
     name="Summarizer Agent",
-    instructions="You are an agent that get a text and provide the summary of it. If you think the text is to long to provide a good summary you can split, do the summary of the chunks and than put all togheter. At the end, if the summary contains some formulas, you must ensure a valid format of them",
+    instructions="You are an agent that get a text and provide the summary of it. If you think the text is to long to provide a good summary you can split, do the summary of the chunks and than put all together. At the end, if the summary contains some formulas, you must ensure a valid format of them",
     tool_manager=manager,
 )
 
