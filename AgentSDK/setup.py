46660387240617002dc0e5a062e278e7ec023c5d
marcomorandin <emmeemmenove@gmail.com>
2025-05-07 00:15:07 +0200

    missed from previous commit

--- AgentSDK/setup.py
+++ AgentSDK/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="trento_agent_sdk",
     packages=find_packages(),
-    version="0.2.2",
+    version="1.0.0",
     description="A Python SDK for AI agents built from scratch with a simple implementation of the Agent2Agent and ModelContext protocols",
     author="Arcangeli and Morandin",
     python_requires=">=3.8",
